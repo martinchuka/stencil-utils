9d60ed2195740fae66b5aa25379ccf96b05b0d31
martin chuka <martinchuka@gmail.com>
2020-01-05 18:25:20 +0100

    fix:bump eventemiter2

--- src/hooks/base.js
+++ src/hooks/base.js
@@ -1,6 +1,6 @@
 import EventEmitter from 'eventemitter2';
 
-export default class extends EventEmitter {
+export default class extends EventEmitter.EventEmitter2 {
     constructor() {
         super();
         this.body = document.body;
